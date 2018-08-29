a9e1019008066564a732b973f16876f8d89c8a35
Chris DeFelice <christopherdefelice@wi-design.com>
2018-08-29 16:48:11 -0400

    docs(type): type readme and docs page
    Add readme to type project and use for type docs page.

---
+++ packages/tc-type/README.md
@@ -0,0 +1,38 @@
+# Type
+
+Apply consistent typographic styling to project.
+
+## Installation
+
+```bash
+npm install @trend/type
+```
+
+## Default Typography
+
+# Header 1
+## Header 2
+### Header 3
+#### Header 4
+##### Header 5
+###### Header 6
+
+**Paragraph** Lorem ipsum dolor sit amet consectetur adipiscing, elit felis auctor cras tempus ligula, est gravida mi bibendum fermentum. Suspendisse erat turpis volutpat molestie et dignissim interdum, eu hac sagittis ultrices vitae vestibulum ligula placerat, blandit tempor arcu orci mi pellentesque. Aliquam nisi hac aliquet posuere ullamcorper proin quam nunc quisque, lobortis facilisi dapibus nibh iaculis semper nam vestibulum, scelerisque montes facilisis pretium cubilia cursus turpis risus.
+
+## Helpers
+
+Adjust typographic appearance of any HTML element with `tc-type` helper class.
+
+```markup
+  <span class="tc-type-h1">...</span>
+  <span class="tc-type-h2">...</span>
+  <span class="tc-type-h3">...</span>
+  <span class="tc-type-h4">...</span>
+  <span class="tc-type-h5">...</span>
+  <span class="tc-type-h6">...</span>
+  <span class="tc-type-subtitle1">...</span>
+  <span class="tc-type-subtitle2">...</span>
+  <span class="tc-type-body1">...</span>
+  <span class="tc-type-body2">...</span>
+  <span class="tc-type-caption">...</span>
+```
--- src/pages/TypePage/index.js
+++ src/pages/TypePage/index.js
@@ -1,58 +1,27 @@
-import React, { Fragment, PureComponent } from 'react';
+import React from 'react';
 
-import Code from 'components/Code';
 import Markdown from 'components/Markdown';
-import examples from './examples.md';
-console.log(examples);
+import readme from 'packages/tc-type/README.md';
 
-const styles = {
-  border: '1px solid #efefef',
-  padding: '1.5rem',
-  backgroundColor: '#efefef'
-};
+function TypePage() {
+  return <div className="tc-mal">
+    <Markdown source={readme} />
 
-const headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    <p className="tc-type-subtitle1">
+      Example <strong>subtitle1</strong> text.
+    </p>
+    <p className="tc-type-subtitle2">
+      Example <strong>subtitle2</strong> text.
+    </p>
+    <p className="tc-type-caption">Example <strong>caption</strong> text.</p>
 
-function Header({ children, elem }) {
-  return React.createElement(elem, {}, children);
-}
-
-class TypePage extends PureComponent {
-  render() {
-    return <Fragment>
-      <p>Apply consistent typographic styling to project.</p>
-      <div style={styles}>
-        { headers.map(header => {
-            return <Header elem={header} key={headers.indexOf(header)}>
-              Header {headers.indexOf(header) + 1}
-            </Header>;
-          })
-        }
-        <p>
-          <strong>Paragraph</strong>.  Lorem ipsum dolor sit amet consectetur adipiscing, elit felis auctor cras tempus ligula, est gravida mi bibendum fermentum. Suspendisse erat turpis volutpat molestie et dignissim interdum, eu hac sagittis ultrices vitae vestibulum ligula placerat, blandit tempor arcu orci mi pellentesque. Aliquam nisi hac aliquet posuere ullamcorper proin quam nunc quisque, lobortis facilisi dapibus nibh iaculis semper nam vestibulum, scelerisque montes facilisis pretium cubilia cursus turpis risus.
-        </p>
-      </div>
-
-      <div style={styles}>
-        <h1>Helpers</h1>
-        <p>
-          Adjust typographic appearance of any HTML element with <code>
-          tc-type</code> helper class.
-        </p>
-        <Markdown source={examples} />
-        <p className="tc-type-subtitle1">Example subtitle1 text.</p>
-        <p className="tc-type-subtitle2">Example subtitle2 text.</p>
-        <p className="tc-type-caption">Example caption text.</p>
-
-        <p className="tc-type-body1">
-          <strong>Example body1 text</strong>.  Lorem ipsum dolor sit amet consectetur adipiscing, elit felis auctor cras tempus ligula, est gravida mi bibendum fermentum. Suspendisse erat turpis volutpat molestie et dignissim interdum, eu hac sagittis ultrices vitae vestibulum ligula placerat, blandit tempor arcu orci mi pellentesque. Aliquam nisi hac aliquet posuere ullamcorper proin quam nunc quisque, lobortis facilisi dapibus nibh iaculis semper nam vestibulum, scelerisque montes facilisis pretium cubilia cursus turpis risus.
-        </p>
-        <p className="tc-type-body2">
-          <strong>Example body2 text</strong>.  Lorem ipsum dolor sit amet consectetur adipiscing, elit felis auctor cras tempus ligula, est gravida mi bibendum fermentum. Suspendisse erat turpis volutpat molestie et dignissim interdum, eu hac sagittis ultrices vitae vestibulum ligula placerat, blandit tempor arcu orci mi pellentesque. Aliquam nisi hac aliquet posuere ullamcorper proin quam nunc quisque, lobortis facilisi dapibus nibh iaculis semper nam vestibulum, scelerisque montes facilisis pretium cubilia cursus turpis risus.
-        </p>
-      </div>
-    </Fragment>;
-  }
+    <p className="tc-type-body1">
+      <strong>Example body1 text</strong>.  Lorem ipsum dolor sit amet consectetur adipiscing, elit felis auctor cras tempus ligula, est gravida mi bibendum fermentum. Suspendisse erat turpis volutpat molestie et dignissim interdum, eu hac sagittis ultrices vitae vestibulum ligula placerat, blandit tempor arcu orci mi pellentesque. Aliquam nisi hac aliquet posuere ullamcorper proin quam nunc quisque, lobortis facilisi dapibus nibh iaculis semper nam vestibulum, scelerisque montes facilisis pretium cubilia cursus turpis risus.
+    </p>
+    <p className="tc-type-body2">
+      <strong>Example body2 text</strong>.  Lorem ipsum dolor sit amet consectetur adipiscing, elit felis auctor cras tempus ligula, est gravida mi bibendum fermentum. Suspendisse erat turpis volutpat molestie et dignissim interdum, eu hac sagittis ultrices vitae vestibulum ligula placerat, blandit tempor arcu orci mi pellentesque. Aliquam nisi hac aliquet posuere ullamcorper proin quam nunc quisque, lobortis facilisi dapibus nibh iaculis semper nam vestibulum, scelerisque montes facilisis pretium cubilia cursus turpis risus.
+    </p>
+  </div>;
 }
 
 TypePage.displayName = 'TypePage';
