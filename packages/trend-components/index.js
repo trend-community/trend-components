5ef17642f2757d45ee070ccc97d85b23d658298d
Chris DeFelice <christopherdefelice@wi-design.com>
2018-12-03 15:41:48 -0500

    feat(trend-components): main entry file
    Create main entry file for individual react components.

---
+++ packages/trend-components/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from '@trend/button';
+export { default as Drawer } from '@trend/Drawer';
+export { DrawerOverlay } from '@trend/Drawer';
+export { default as FocusTrap } from '@trend/focustrap';
+export { default as withIcon } from '@trend/icon';
+export { default as Topbar } from '@trend/topbar';
