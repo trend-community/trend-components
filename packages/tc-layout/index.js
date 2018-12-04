21657d7e2bf0f5f886d18f6abc3f50ab4c214966
Chris DeFelice <christopherdefelice@wi-design.com>
2018-12-04 15:49:09 -0500

    refactor(layout): src index
    Add index to src directory and remove main index.js file.

--- packages/tc-layout/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LayoutContext } from './src/Context';
-export { default as withLayoutConsumer } from './src/withConsumer';
-export { default } from './src/Provider';
---
+++ packages/tc-layout/src/index.js
@@ -0,0 +1,3 @@
+export { default as LayoutContext } from './Context';
+export { default as withLayoutConsumer } from './withConsumer';
+export { default } from './Provider';
