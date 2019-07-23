ab9b782071d78532510e9e3576c15a476ba9c361
Chris DeFelice <christopherdefelice@wi-design.com>
2019-07-23 16:57:04 -0400

    chore(root): adjust environment varialbe for env module
    Use the NODE_ENV in place TC_ENV enviroment variable for testing.  Add a babel building predicate to module.

--- scripts/env.js
+++ scripts/env.js
@@ -9,16 +9,23 @@ const PRODUCTION = 'production';
 
 function env() {
   function isDev() {
-    return process.env.TC_ENV === DEVELOPMENT;
+    return process.env.NODE_ENV === DEVELOPMENT;
   }
 
   function isProd() {
-    return process.env.TC_ENV === PRODUCTION;
+    return process.env.NODE_ENV === PRODUCTION;
+  }
+
+  function isBabelBuilding() {
+    const BABEL_ENV = process.env.BABEL_ENV;
+
+    return BABEL_ENV !== undefined && BABEL_ENV !== "cjs";
   }
 
   const api = {
     isDev,
-    isProd
+    isProd,
+    isBabelBuilding
   };
 
   function init() {
