b3b3fcc12d5672f12ceb33d6acd37e16950fd27f
Chris DeFelice <christopherdefelice@wi-design.com>
2019-10-16 15:48:55 -0400

    feat(trend-components): add additional packages
    Add packages: disclosure, divider, interactive, list, rover, uitls, and visuallyHidden.

--- packages/trend-components/index.js
+++ packages/trend-components/index.js
@@ -1,7 +1,33 @@
-export { default as Button } from '@trend/button';
+export { default as Button, useButton } from '@trend/button';
+export {
+  default as Disclosure,
+  useDisclosure,
+  useDisclosureState,
+  DisclosureButton,
+  useDisclosureButton
+} from '@trend/button';
+export { default Divider, useDivider } from '@trend/divider';
 export { default as Drawer } from '@trend/Drawer';
 export { DrawerOverlay } from '@trend/Drawer';
 export { default as FocusTrap } from '@trend/focustrap';
 export { default as withIcon } from '@trend/icon';
+export { default as Interactive, useInteractive } from '@trend/interactive';
+export {
+  default as List,
+  useList,
+  useListState,
+  ListItem,
+  useListItem,
+  ListItemChild,
+  useListItemChild,
+  ListDivider,
+  useListDivider
+} from '@trend/list';
+export { default as Rover, useRover, useRoverState } from '@trend/rover';
 export { default as TextField } from '@trend/textfield';
 export { default as Topbar } from '@trend/topbar';
+export * from '@trend/utils';
+export {
+  default as VisuallyHidden,
+  useVisuallyHidden
+} from '@trend/visually-hidden';
