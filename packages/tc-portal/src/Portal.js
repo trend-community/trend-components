ab4eb95ee2ed6b7de6398b36b4382c8111e597fb
Chris DeFelice <christopherdefelice@wi-design.com>
2019-11-26 11:28:27 -0500

    feat(portal): wrap a react portal
    Create new abstract component that wraps a React portal.

--- packages/tc-portal/src/Portal.js
+++ packages/tc-portal/src/Portal.js
@@ -1,1 +1,50 @@
-export function Portal() {};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import cn from 'classnames';
+
+import isNil from '@trend/utils/internal/isNil';
+import not from '@trend/utils/internal/not';
+
+const isNotUndefined = not(isNil);
+
+const PortalCtx = React.createContext(
+  isNotUndefined(typeof document) ? document.body : null
+);
+
+function Portal({ children, className }) {
+  const ctx = React.useContext(PortalCtx);
+  const [container] = React.useState(() => {
+    let container = null;
+
+    if (isNotUndefined(typeof document)) {
+      const portal = document.createElement('div');
+      portal.className = cn(Portal.__defaultClassName__, className);
+
+      container = portal;
+    }
+
+    return container;
+  });
+
+  React.useEffect(() => {
+    if (!container || !ctx) {
+      return undefined;
+    }
+
+    ctx.appendChild(container);
+
+    return () => {
+      ctx.removeChild(container);
+    }
+  }, [container, ctx]);
+
+  return container
+    ? <PortalCtx.Provider value={container}>
+        {ReactDOM.createPortal(children, container)}
+      </PortalCtx.Provider>
+    : null;
+};
+
+Portal.__defaultClassName__ = 'tc-Portal';
+
+export default Portal;
